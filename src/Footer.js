f774e1506c6e3030c5d36d28210d26a770eda351
KumaranPunniyakotti <kumaranpunniyakotti@gmail.com>
2021-09-22 19:45:20 +0530

    gotta add this too

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+  
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className= "TITLE"> 
+        <h4><center>Powered by HoBaAlBi's</center></h4>
+        </div>
+    )
+}
+
+export default Footer
